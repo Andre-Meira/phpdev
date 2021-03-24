8b9e4aade086b510cd074114a098128d4c21cc16
Andre-Meira <andre._.meira@hotmail.com>
2021-03-24 15:16:13 -0400

    add call method

--- app/Dispatch.php
+++ app/Dispatch.php
@@ -11,6 +11,11 @@ class Dispatch extends ClassRouter
     private $Param = [];
     private $Obj;
 
+    protected function getMethod(){return $this->method;}
+    public function setMethod($method){$this->method = $method;}
+    protected function getParam(){return $this->Param;}
+    public function setParam($Param){$this->Param = $Param;}
+
     #Metodo Contrustor;
     Public function __construct()
     {
@@ -18,12 +23,33 @@ class Dispatch extends ClassRouter
     }
 
     #metodo de Edição de Controller;
-    public function addController()
+    private function addController()
     {
         $RotaController = $this->getRota();
         $NameS = "App\\Controller\\{$RotaController}";
         $this->Obj = new $NameS; // OBJ = App\Controller\
 
+        if($this->parseUrl()[1]){
+            $this->addMethod();
+        }
     }
+
+    private function addMethod() 
+    {
+        if(method_exists($this->Obj, $this->parseUrl()[1])){
+            $this->setMethod("{$this->parseUrl()[1]}");
+            $this->addParam();
+            call_user_func_array([$this->Obj, $this->getMethod()], $this->getParam());
+        }
+        else{
+            echo "Este Param não existe";
+        }
+    }
+
+    private function addParam() 
+    {
+
+    }
+
 
 }
